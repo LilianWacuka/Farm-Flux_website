a2ae5d2ff2493cfd4ed5f684f070de0913f180a5
LilianWacuka <liliannguruw@gmail.com>
2025-07-24 11:29:32 +0300

    Fixed Cors

--- backend/server.js
+++ backend/server.js
@@ -21,17 +21,7 @@ const allowedOrigins = [
   'https://farm-flux-website.onrender.com'
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
